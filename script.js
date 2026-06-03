34565230ada8699d8ab734d198df987085c18764
Nikows <44471@aeaag.pt>
2026-06-03 19:23:29 +0100

    Add functions to increment and decrement a number

---
+++ script.js
@@ -0,0 +1,14 @@
+let messageEl = document.getElementById("message");
+let numberEl = document.getElementById("number");
+function addNumbers() {
+  let num = parseInt(numberEl.textContent || "0", 10);
+  numberEl.textContent = num + 1;
+}
+function subNumbers() {
+  let num = parseInt(numberEl.textContent || "0", 10);
+  if (num <= 0) {
+    messageEl.textContent = "You cannot subtract any further.";
+  } else {
+    numberEl.textContent = num - 1;
+  }
+}
