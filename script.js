276d795cd201fb45b8c9cb32bd5b51011e593481
Nikows <44471@aeaag.pt>
2026-06-03 19:28:02 +0100

    Rename functions and update messages in script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let messageEl = document.getElementById("message");
 let numberEl = document.getElementById("number");
-function addNumbers() {
+function addNumber() {
   let num = parseInt(numberEl.textContent || "0", 10);
   numberEl.textContent = num + 1;
 }
-function subNumbers() {
+function subNumber() {
   let num = parseInt(numberEl.textContent || "0", 10);
   if (num <= 0) {
-    messageEl.textContent = "You cannot subtract any further.";
+    messageEl.textContent = "You cannot decrement any further.";
   } else {
     numberEl.textContent = num - 1;
   }
