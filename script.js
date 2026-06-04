e11e80c0b1548d40ee7e735677d80a06be7f5bf2
Nikows <44471@aeaag.pt>
2026-06-04 10:48:46 +0100

    Standardize formatting in addNumber and subNumber functions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let messageEl = document.getElementById("message");
 let numberEl = document.getElementById("number");
 function addNumber() {
-  let num = parseInt(numberEl.textContent || "0", 10);
-  numberEl.textContent = num + 1;
+    let num = parseInt(numberEl.textContent || "0", 10);
+    numberEl.textContent = num + 1;
 }
 function subNumber() {
-  let num = parseInt(numberEl.textContent || "0", 10);
-  if (num <= 0) {
-    messageEl.textContent = "You cannot decrement any further.";
-    setTimeout(() => {messageEl.textContent = ""}, 1000);
-  } else {
-    numberEl.textContent = num - 1;
-  }
+    let num = parseInt(numberEl.textContent || "0", 10);
+    if (num <= 0) {
+        messageEl.textContent = "You cannot decrement any further.";
+        setTimeout(() => {messageEl.textContent = ""}, 1000);
+    } else {
+        numberEl.textContent = num - 1;
+    }
 }
