9b72d4e4738a62fa5882fbac55dfd93bf16e339b
Nikows <44471@aeaag.pt>
2026-06-04 20:17:38 +0100

    Notify user when number reaches 2026
    Added a check to display a message when the number reaches 2026

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let numberEl = document.getElementById("number");
 function addNumber() {
     let num = parseInt(numberEl.textContent || "0", 10);
     numberEl.textContent = num + 1;
+    if (num === 2025) {
+        messageEl.textContent = "You've reached the current year!";
+        setTimeout(() => {messageEl.textContent = ""}, 1000);
+    }
 }
 function subNumber() {
     let num = parseInt(numberEl.textContent || "0", 10);
@@ -11,5 +15,9 @@ function subNumber() {
         setTimeout(() => {messageEl.textContent = ""}, 1000);
     } else {
         numberEl.textContent = num - 1;
+    }
+    if (num === 2027) {
+        messageEl.textContent = "You've reached the current year!";
+        setTimeout(() => {messageEl.textContent = ""}, 1000);
     }
 }
